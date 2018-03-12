e7aba80f434bc8a0895eb0996b42b20fcc93c19e
carlosSusarrey <carlos.susarrey.diaz@gmail.com>
2018-03-11 22:48:18 -0600

    Created gameCanvas

---
+++ Rishadan/Rishadan/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Rishadan
+{
+    partial class gameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gameCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameCanvas
+            // 
+            this.gameCanvas.Location = new System.Drawing.Point(12, 12);
+            this.gameCanvas.Name = "gameCanvas";
+            this.gameCanvas.Size = new System.Drawing.Size(660, 358);
+            this.gameCanvas.TabIndex = 0;
+            this.gameCanvas.TabStop = false;
+            this.gameCanvas.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // gameWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 382);
+            this.Controls.Add(this.gameCanvas);
+            this.Name = "gameWindow";
+            this.Text = "Game of life";
+            this.Load += new System.EventHandler(this.gameWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gameCanvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox gameCanvas;
+    }
+}
+
